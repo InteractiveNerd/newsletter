464ac55272f6121d1c95a7b0710ed11fafe8c1ef
Henry Richardson <hrichardson83@gmail.com>
2019-01-28 12:26:13 -0500

    mobile layout complete

--- discovery-ui/src/components/pages/Email.js
+++ discovery-ui/src/components/pages/Email.js
@@ -6,7 +6,8 @@ class Email extends Component {
     this.state = {
       firstName: 'First Name',
       lastName: 'Last Name',
-      email: 'enter email address',
+      emailText: 'enter email address',
+      email: '',
       gdpr: ''
     }
 
@@ -15,14 +16,14 @@ class Email extends Component {
   }
   
   handleChange = (e) => {
-      this.setState({
-          [e.target.id]: e.target.value
-      })
+    this.setState({
+        [e.target.id]: e.target.value
+    })
   }
   handleSumbit = (e) => {
-      e.preventDefault();
-      this.props.signUp(this.state)
-      // console.log(this.state)
+    e.preventDefault();
+    // this.props.signUp(this.state)
+    console.log(this.state)
   }
   render() {
     return (
@@ -33,10 +34,10 @@ class Email extends Component {
               <p>Join The List:</p>
             </div>
             <div className="subTitle">
-              <p><span class="aw">Sign up for</span> <span class="aw">the TLC newsletter</span></p>
+              <p><span className="aw">Sign up for</span> <span className="aw">the TLC newsletter</span></p>
             </div>
             <div className="email">
-              <input type="email" id="email" value={this.state.email} onChange={this.handleChange} />
+              <input type="email" id="email" placeholder={this.state.emailText} value={this.state.email} onChange={this.handleChange} />
             </div>
             <div className="submitBtn">
               <input type="submit" value="Next" />
@@ -44,8 +45,8 @@ class Email extends Component {
             <div className="gdpr">
               <div><input id="gdpr" name="isGoing" type="checkbox" checked={this.state.isGoing} onChange={this.handleInputChange} /></div>
               <div>
-                <label for="gdpr">
-                  <span class="aw">I agree to receive information from</span> <span class="aw">Discovery Communications in accordance</span> <span class="aw">with the following <a href="https://www.google.com/" >Privacy Policy</a></span>
+                <label htmlFor="gdpr">
+                  <span className="aw">I agree to receive information from</span> <span className="aw">Discovery Communications in accordance</span> <span className="aw">with the following <a href="https://www.google.com/" >Privacy Policy</a></span>
                 </label>
               </div>
             </div>
--- discovery-ui/src/components/pages/ThankYou.js
+++ discovery-ui/src/components/pages/ThankYou.js
@@ -5,14 +5,16 @@ class ThankYou extends Component {
   render() {
     return (
       <div className="container">
-        <div className="title">
-          <p>congratulations!</p>
-        </div>
-        <div className="subTitle">
-          <p>Thank You For Signing Up!</p>
-        </div>
-        <div className="bodyCopy">
-          <p>Look out for the latest news on your favorite shows.</p>
+        <div className="row">
+          <div className="title">
+            <p>congratulations!</p>
+          </div>
+          <div className="subTitle">
+            <p><span className="aw">Thank You For</span> <span className="aw">Signing Up!</span></p>
+          </div>
+          <div className="bodyCopy">
+            <p>Look out for the latest news on your favorite shows.</p>
+          </div>
         </div>
       </div>
     )
