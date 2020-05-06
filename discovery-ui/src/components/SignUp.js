26b59c311949d99d7cbaa92f895af6a9a9f0e083
Henry Richardson <hrichardson83@gmail.com>
2020-05-06 18:18:09 -0400

    formik added

--- discovery-ui/src/components/SignUp.js
+++ discovery-ui/src/components/SignUp.js
@@ -2,36 +2,7 @@ import React, { useState } from 'react';
 import Title from './atoms/Title.js';
 import SubmitBtn from './atoms/SubmitBtn.js';
 import { useFormik } from 'formik';
-// import * as Yup from 'yup';
-
-const validate = (values) => {
-  const errors = {};
-  if (!values.firstName) {
-    errors.firstName = 'Required';
-  } else if (values.firstName.length > 15) {
-    errors.firstName = 'Must be 15 characters or less';
-  }
-
-  if (!values.lastName) {
-    errors.lastName = 'Required';
-  } else if (values.lastName.length > 20) {
-    errors.lastName = 'Must be 20 characters or less';
-  }
-
-  if (!values.email) {
-    errors.email = 'Required';
-  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
-    errors.email = 'Invalid Email Address';
-  }
-
-  if (!values.agree) {
-    errors.email = 'Required';
-  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
-    errors.email = 'Invalid Email Address';
-  }
-
-  return errors;
-};
+import * as Yup from 'yup';
 
 const SignUp = () => {
   const formik = useFormik({
@@ -41,7 +12,12 @@ const SignUp = () => {
       lastName: '',
       agree: 'false',
     },
-    validate,
+    validationSchema: Yup.object({
+      email: Yup.string().email('Invalid emil address').required('Required'),
+      firstName: Yup.string().max(15, 'Must be 15 characters or less').required('Required'),
+      lastName: Yup.string().max(20, 'Must be 15 characters or less').required('Required'),
+      agree: Yup.boolean().required('Required').oneOf([true], 'you must except the terms'),
+    }),
     onSubmit: (values) => {
       console.log('submit', JSON.stringify(values, null, 2));
     },
