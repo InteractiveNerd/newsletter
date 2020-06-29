0e2cd2300d0273a9e03555cd42ca406f094ad3b9
Noximus <hrichardson83@gmail.com>
2020-06-29 10:10:21 -0400

    adding unit testing

--- cypress/integration/examples/newsletter.spec.js
+++ cypress/integration/examples/newsletter.spec.js
@@ -1,15 +1,16 @@
-describe('My First Test', function () {
-  it('Does not do much!', function () {
-    expect(true).to.equal(true);
-  });
-});
-describe('My Second Test', function () {
-  it('Visits my local site', function () {
+describe('NewsLetter Walkthrough Test', function () {
+  it('Submits Fake User Info', function () {
     cy.visit('http://localhost:3000/');
     cy.get('#email').type('fake@gmail.com').should('have.value', 'fake@gmail.com');
+    cy.wait(1500);
+    cy.get('#gdpr').check();
+    cy.wait(1500);
     cy.contains('next').click();
+    cy.wait(1500);
     cy.get('#firstName').type('henry').should('have.value', 'henry');
+    cy.wait(1500);
     cy.get('#lastName').type('richardson').should('have.value', 'richardson');
+    cy.wait(1500);
     cy.contains('sign up').click();
   });
 });
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { render } from '@testing-library/react';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('First Test', () => {
+  test('works', () => {
+    const { getByText } = render(<App />);
+    const linkElement = getByText(/sign up for the IN newsletter./i);
+    expect(linkElement).toBeInTheDocument();
+  });
 });
