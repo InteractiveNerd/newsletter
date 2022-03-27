c9548029b1c517d3f6e9cf19996a5714c53a49e4
Noximus <hrichardson83@gmail.com>
2022-03-27 16:21:57 -0400

    update

--- src/components/atoms/SubmitBtn.js
+++ src/components/atoms/SubmitBtn.js
@@ -3,11 +3,11 @@ import React from 'react';
 const SubmitBtn = (props) => {
   const { text } = props;
   return (
-    <>
+    <React.Fragment>
       <div className="submitBtn">
         <input type="submit" value={text} />
       </div>
-    </>
+    </React.Fragment>
   );
 };
 
--- src/components/atoms/Title.js
+++ src/components/atoms/Title.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 export default function Title({ text, type: titleType }) {
   return (
-    <>
+    <React.Fragment>
       <div className={titleType}>
         <p>{text}</p>
       </div>
-    </>
+    </React.Fragment>
   );
 }
